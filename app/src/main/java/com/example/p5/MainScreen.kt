a5d13afa663e85600a07c86b097f1b4c9439c124
hariwr <hariwr25@gmail.com>
2024-10-30 16:19:13 +0700

    Fix Layout

--- app/src/main/java/com/example/p5/MainScreen.kt
+++ app/src/main/java/com/example/p5/MainScreen.kt
@@ -33,89 +33,155 @@ fun MainScreen(
 ) {
     var nama by rememberSaveable { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
-    var alamat by remember { mutableStateOf("") }
+    var alamat by rememberSaveable { mutableStateOf("") }
     var noHP by remember { mutableStateOf("") }
     var selectedGender by remember { mutableStateOf("") }
 
     val jenisKelamin = listOf("Laki-Laki", "Perempuan")
+
     var namaUser by rememberSaveable { mutableStateOf("") }
     var emailUser by remember { mutableStateOf("") }
-    var alamatUser by remember { mutableStateOf("") }
+    var alamatUser by rememberSaveable { mutableStateOf("") }
     var noHPUser by remember { mutableStateOf("") }
     var selectedGenderUser by remember { mutableStateOf("") }
 
-    Column (Modifier
-        .fillMaxSize()
-        .padding(16.dp),
+    Column (
+        Modifier
+            .fillMaxSize()
+            .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+
+    ) {
+        Text(text = "Data Diri")
+
         OutlinedTextField(
+
             value = nama,
             onValueChange = {nama = it},
-            placeholder = { Text("Masukkan Nama")},
-            label = {Text("Nama")},
-            modifier = Modifier.fillMaxWidth().padding(5.dp))
-        Row (){
-            jenisKelamin.forEach { item ->
-            Row (verticalAlignment = Alignment.CenterVertically){ RadioButton(selected = selectedGender == item,
-                onClick = {
-                    selectedGender = item
-                })
-            Text(item)
+            placeholder = { Text("Masukkan Nama Anda")},
+            label = {Text("Nama" )},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+
+
+        )
+
+        Row {
+            jenisKelamin.forEach{item ->
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = selectedGender == item,
+                        onClick = {
+                            selectedGender = item
+                        })
+                    Text(item)
                 }
+
             }
+
         }
 
         OutlinedTextField(
+
             value = email,
             onValueChange = {email = it},
-            placeholder = { Text("Masukkan Email")},
+            placeholder = { Text("Masukkan Email Anda")},
+            label = {Text("Email" )},
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            label = {Text("Email")},
-            modifier = Modifier.fillMaxWidth().padding(5.dp))
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+
+
+        )
 
         OutlinedTextField(
+
             value = alamat,
             onValueChange = {alamat = it},
-            placeholder = { Text("Masukkan Alamat")},
-            label = {Text("Alamat")},
-            modifier = Modifier.fillMaxWidth().padding(5.dp))
+            placeholder = { Text("Masukkan Alamat Anda")},
+            label = {Text("Alamat" )},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+
+
+        )
 
         OutlinedTextField(
+
             value = noHP,
             onValueChange = {noHP = it},
-            placeholder = { Text("Masukkan No HP")},
-            label = {Text("No HP")},
+            placeholder = { Text("Masukkan NoHP Anda")},
+            label = {Text("No HP" )},
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth().padding(5.dp))
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+
+
+        )
 
         Button(onClick = {
-        })
-        { Text("Simpan") }
+            namaUser = nama
+            selectedGenderUser = selectedGender
+            emailUser = email
+            alamatUser = alamat
+            noHPUser = noHP
+        },
+
+            ) {
+            Text("Simpan Data")
+        }
+
+        Card(modifier = Modifier.size(height = 200.dp, width = 350.dp)) {
+            Column {
+                CardSection(judulParam = "Nama", isiParam = namaUser)
+                CardSection(judulParam = "Jenis Kelamin", isiParam = selectedGenderUser)
+                CardSection(judulParam = "Email", isiParam = emailUser)
+                CardSection(judulParam = "Alamat", isiParam = alamatUser)
+                CardSection(judulParam = "No HP", isiParam = noHPUser)
+            }
+
+        }
+
     }
 
-    Card (modifier.size(height = 200.dp, width = 300.dp)){
-        CardSection(judulParam = "Nama", isiParam = namaUser)
-        CardSection(judulParam = "Jenis Kelamin", isiParam = selectedGenderUser)
-        CardSection(judulParam = "Alamat", isiParam = alamatUser)
-        CardSection(judulParam = "No HP", isiParam = noHPUser)
-    }
+
 }
 
+
 @Composable
-fun CardSection(judulParam:String, isiParam:String) {
+fun CardSection(
+    judulParam: String,
+    isiParam: String
+
+){
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = Modifier.padding(5.dp)
     ) {
 
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-            Text(text = judulParam, modifier = Modifier.weight(0.8f))
-            Text(text = ":", modifier = Modifier.weight(0.2f))
-            Text(text = isiParam, modifier = Modifier.weight(2f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
 
+        ) {
+            Text(
+                text = judulParam,
+                modifier = Modifier.weight(0.8f)
+            )
+            Text(
+                text = " : ",
+                modifier = Modifier.weight(0.2f)
+            )
+            Text(
+                text = "$isiParam",
+                modifier = Modifier.weight(2f)
+            )
         }
-
     }
 
 }
